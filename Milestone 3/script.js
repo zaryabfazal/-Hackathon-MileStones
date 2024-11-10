67e458cfa1906ea333b89f88e0989ef276ff4951
zaryabfazal <zaryabfazalhussain@gmail.com>
2024-11-10 19:26:58 +0500

    Add files via upload

---
+++ Milestone 3/script.js
@@ -0,0 +1,24 @@
+// Get refrences to the form and display area 
+var form = document.getElementById("resume-form");
+var resumeDisplayElement = document.getElementById('resume-display');
+// handl form submission 
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent page reload
+    // collect input values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    // generate the resume content dynamically 
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>phone:</b>").concat(phone, "</p>\n \n <h3>Education</h3>\n <p>").concat(education, "</p>\n \n <h3>Experience</h3>\n <p>").concat(experience, "</p>\n\n <h3>Skills</h3>\n <p>").concat(skills, "</p>");
+    // display the generated resume 
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else
+        [
+            console.error('the resume element is missing')
+        ];
+});
